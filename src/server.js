86c90877f06f6087de8f953e53785de5abb56847
Haedong Kim <haedong1111@gmail.com>
2022-04-21 19:59:51 +0900

    JS: Add logger

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import livereload from 'livereload';
 import connectLivereload from 'connect-livereload';
 import router from './router';
+import morgan from 'morgan';
 
 // Live Reload
 const liveReloadServer = livereload.createServer({
@@ -10,12 +11,16 @@ const liveReloadServer = livereload.createServer({
 });
 liveReloadServer.watch(process.cwd() + '/src');
 
+// Logger
+const logger = morgan('dev');
+
 // App settings
 const app = express();
 app.use(connectLivereload());
 app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views/screens');
 app.use('/public', express.static(process.cwd() + '/src/public'));
+app.use(logger);
 app.use('/', router);
 
 export default app;
