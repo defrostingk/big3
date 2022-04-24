5c3366fd3c329c1fd2a23ebee341e283498fe44b
Haedong Kim <haedong1111@gmail.com>
2022-04-24 16:36:22 +0900

    JS: Switch screen of break time

--- src/public/js/stopwatch.js
+++ src/public/js/stopwatch.js
@@ -1,6 +1,11 @@
 const stopwatchTime = document.querySelector('.stopwatch__time');
 const stopwatchTimePure = document.querySelector('.stopwatch__time--pure');
 
+const navbar = document.querySelector('nav');
+const navbarMenus = navbar.querySelectorAll('a');
+const breakBtn = document.querySelector('.break-btn');
+const breakEndBtn = document.querySelector('.break-end-btn');
+
 const TITLE_STOPWATCH = 'Stopwatch';
 const TITLE_Break = 'Break';
 const TITLE_RECORDS = 'Records';
@@ -13,18 +18,35 @@ const TITLE_RECORDS = 'Records';
 // quit 정지
 // home 작동 중이라면 현재 시간 저장, 다시 들어올 때 (현재 시간-과거 시간)
 // 작동 중이 아니라면 다시 들어올 때 (과거시간)
-switchScreen(TITLE_STOPWATCH);
-// switchScreen(TITLE_Break);
-// switchScreen(TITLE_RECORDS);
+
 initStopwatch();
+
+// Select navbar
+navbarMenus.forEach((navbarMenu) =>
+  navbarMenu.addEventListener('click', () => {
+    const navbarMenuTitle = navbarMenu.querySelector('span');
+    switchScreen(navbarMenuTitle.innerText);
+  })
+);
+
+// Start break
+breakBtn.addEventListener('click', () => {
+  switchScreen(TITLE_Break);
+});
+
+breakEndBtn.addEventListener('click', () => {
+  switchScreen(TITLE_STOPWATCH);
+});
 
 function switchScreen(screen) {
   const containerStopwatch = document.querySelector('.stopwatch__container');
   const containerBreak = document.querySelector('.break__container');
   const containerRecords = document.querySelector('.records__container');
+
   containerStopwatch.style.display = 'none';
   containerBreak.style.display = 'none';
   containerRecords.style.display = 'none';
+
   if (screen === TITLE_STOPWATCH) {
     setHeaderTitle(TITLE_STOPWATCH);
     containerStopwatch.style.display = 'flex';
@@ -39,10 +61,7 @@ function switchScreen(screen) {
 
 function setHeaderTitle(title) {
   const headerTitle = document.querySelector('.header__title');
-  const navbar = document.querySelector('nav');
   headerTitle.innerText = title;
-
-  const navbarMenus = navbar.querySelectorAll('a');
 
   navbarMenus.forEach((navbarMenu) => {
     const navbarMenuTitle = navbarMenu.querySelector('span');
@@ -56,6 +75,7 @@ function setHeaderTitle(title) {
 }
 
 function initStopwatch() {
+  switchScreen(TITLE_STOPWATCH);
   stopwatchTime.innerText = '00:00:00.00';
   stopwatchTimePure.innerText = '00:00:00.00';
 }
