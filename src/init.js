e7fdf873ec5185dda789e837dc6a9f9b40c5c075
Haedong Kim <haedong1111@gmail.com>
2022-04-25 20:55:56 +0900

    JS: Server listening at heroku app

--- src/init.js
+++ src/init.js
@@ -9,5 +9,10 @@ const PORT = process.env.PORT || LOCAL_PORT;
 app.listen(PORT, () => {
   if (PORT === LOCAL_PORT) {
     console.log(`Server listening on port http://localhost:${PORT}`);
+  } else if (PORT === process.env.PORT) {
+    console.log(`Server listening on port ${process.env.PORT}`);
+    console.log(
+      "You can use 'Big3' at https://big3-workout-manager.herokuapp.com/"
+    );
   }
 });
