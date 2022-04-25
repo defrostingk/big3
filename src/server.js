e4380fa52c0b6623939323afd31b2ad7f4aeee23
Haedong Kim <haedong1111@gmail.com>
2022-04-25 17:19:48 +0900

    JS: Delete livereload server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,10 @@
 import express from 'express';
 import session from 'express-session';
-import livereload from 'livereload';
-import connectLivereload from 'connect-livereload';
 import rootRouter from './routers/rootRouter';
 import settingsRouter from './routers/settingsRouter';
 import morgan from 'morgan';
 import MongoStore from 'connect-mongo';
 import { localsMiddleware } from './middlewares';
-
-// Live Reload
-const liveReloadServer = livereload.createServer({
-  exts: ['pug', 'js', 'css'],
-});
-liveReloadServer.watch(process.cwd() + '/src');
 
 // Logger
 const logger = morgan('dev');
@@ -23,7 +15,6 @@ app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views/screens');
 
 // Middlewares
-app.use(connectLivereload());
 app.use('/static', express.static(process.cwd() + '/src/public'));
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
