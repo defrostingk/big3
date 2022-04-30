d2eac829144325eca348d0f30bc6d3206ba6f0e4
Haedong Kim <haedong1111@gmail.com>
2022-04-30 18:28:08 +0900

    JS: Save workoutRecords in db

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,9 +1,28 @@
+import express from 'express';
+import { Workout } from '../models/Records';
+import User from '../models/User';
+
+const sectionTitle = 'Workout';
+const ERROR_FAILED_TO_SAVE = 'Failed to save.';
+
 export function getWorkout(req, res) {
-  return res.render('workout', { sectionTitle: 'Workout' });
+  return res.render('workout', { sectionTitle });
 }
 
-export function postWorkout(req, res) {
-  console.log(req.body);
-  console.log(req.body.workout['0'].sets);
-  return res.render('workout', { sectionTitle: 'Workout' });
+export async function postWorkout(req, res) {
+  const workoutRecordsObj = req.body;
+  const { _id } = res.locals.loggedInUser;
+  try {
+    const user = await User.findById(_id);
+    const workoutRecords = await Workout.create(workoutRecordsObj);
+    user.workoutRecords.push(workoutRecords._id);
+    user.save();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .render('workout', { sectionTitle, errorMessage: ERROR_FAILED_TO_SAVE });
+  }
+
+  return res.render('workout', { sectionTitle });
 }
